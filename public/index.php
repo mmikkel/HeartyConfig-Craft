1f4d046532d65a1f9b259f3aaa30fd389f43f047
M. Mikkel Rummelhoff <mail@matsmikkel.net>
2015-09-08 08:22:08 +0200

    Updated index.php as per Craft 2.4.2691

--- public/index.php
+++ public/index.php
@@ -14,7 +14,12 @@ $path = rtrim($craftPath, '/').'/app/index.php';
 
 if (!is_file($path))
 {
-	exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
+    if (function_exists('http_response_code'))
+    {
+        http_response_code(503);
+    }
+
+    exit('Could not find your craft/ folder. Please ensure that <strong><code>$craftPath</code></strong> is set correctly in '.__FILE__);
 }
 
 require_once $path;
