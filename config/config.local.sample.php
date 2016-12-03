7f8a49801107cba9f46523a8cc802820d206d0d6
Mats Mikkel Rummelhoff <mail@mmikkel.no>
2016-12-03 09:51:45 +0100

    Tab fix for local config sample

--- config/config.local.sample.php
+++ config/config.local.sample.php
@@ -16,14 +16,14 @@ return array(
 
 	'devMode' => true,
 	'phpMaxMemoryLimit' => '256M',
-    'backupDbOnUpdate' => true,
-    'translationDebugOutput' => false,
-    'useCompressedJs' => false,
-    'enableTemplateCaching' => false,
-    'userSessionDuration'           => 'P101Y',
+  'backupDbOnUpdate' => true,
+  'translationDebugOutput' => false,
+  'useCompressedJs' => false,
+  'enableTemplateCaching' => false,
+  'userSessionDuration'           => 'P101Y',
 	'rememberedUserSessionDuration' => 'P101Y',
 	'rememberUsernameDuration'      => 'P101Y',
-	
+
 	//'testToEmailAddress'  => 'tester@testingtesting123.com',
 
 );
